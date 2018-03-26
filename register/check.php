57efec89e8aa5fcf55ae831705a5af89fb5f385d
ei17ringo <ei17ringo@gmail.com>
2018-03-26 11:15:30 +0800

    確認画面  書き直し処理  不正アクセスブロック Thanksページ　完成

--- register/check.php
+++ register/check.php
@@ -1,18 +1,89 @@
 <?php
-session_start();
+    session_start();
+
+    //セッション変数に入力された値が保存されていない場合、signup.phpへリダイレクト
+    if (!isset($_SESSION['register'])){
+      header('Location: signup.php');
+      exit();
+    }
+
+
+    // ①
+    $name = $_SESSION['register']['name'];
+    $email = $_SESSION['register']['email'];
+    $password = $_SESSION['register']['password'];
+    $img_name = $_SESSION['register']['img_name'];
+
+    //登録ボタンが押された時
+    if (isset($_POST) && !empty($_POST)){
+
+      //データベース接続
+      require("../dbconnect.php");
+
+      // INSERT文作成
+      $sql = 'INSERT INTO `users` SET `name`=?, `email`=?, `password`=?, `img_name`=?, `created`=NOW()';
+
+      // INSERT文実行
+      $data = array($name,$email,password_hash($password,PASSWORD_DEFAULT),$img_name);
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
+
+      //使い終わったSESSION変数を破棄
+      unset($_SESSION['register']);
+
+      // thanks.phpへ移動
+      header("Location: thanks.php");
+      exit();
+
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Learn SNS</title>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 </head>
-<body>
-  <?php echo $_SESSION['register']['name']; ?><br>
-  <?php echo $_SESSION['register']['email']; ?><br>
-  <?php echo $_SESSION['register']['password']; ?><br>
-  <?php echo $_SESSION['register']['img_name']; ?><br>
-
-  <img src="../user_profile_img/<?php echo $_SESSION['register']['img_name']; ?>" width="60">
+<body style="margin-top: 60px">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2 thumbnail">
+        <h2 class="text-center content_header">アカウント情報確認</h2>
+        <div class="row">
+          <div class="col-xs-4">
+            <img src="../user_profile_img/<?php echo htmlspecialchars($img_name); ?>" class="img-responsive img-thumbnail">
+          </div>
+          <div class="col-xs-8">
+            <div>
+              <span>ユーザー名</span>
+              <p class="lead"><?php echo htmlspecialchars($name); ?></p>
+            </div>
+            <div>
+              <span>メールアドレス</span>
+              <p class="lead"><?php echo htmlspecialchars($email); ?></p>
+            </div>
+            <div>
+              <span>パスワード</span>
+              <!-- ② -->
+              <p class="lead">●●●●●●●●</p>
+            </div>
+            <!-- ③ -->
+            <form method="POST" action="">
+              <!-- ④ -->
+              <a href="signup.php?action=rewrite" class="btn btn-default">&laquo;&nbsp;戻る</a> | 
+              <!-- ⑤ -->
+              <input type="hidden" name="action" value="submit">
+              <input type="submit" class="btn btn-primary" value="ユーザー登録">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/js/jquery-3.1.1.js"></script>
+  <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
